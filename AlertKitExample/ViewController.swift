afcc29b5432a15f7941de1c6be0a82ed8aea5572
Nathan Tannar <nathantannar4@gmail.com>
2017-10-19 21:29:27 -0700

    Fork NTPing & NTToast from NTComponents

---
+++ AlertKitExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AlertKitExample
+//
+//  Created by Nathan Tannar on 10/19/17.
+//  Copyright © 2017 Nathan Tannar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
